a3df0b02638d0a1a5ddce9c6fc81d39aa043c085
Billy <billy.vu67@gmail.com>
2025-03-04 22:20:51 +0100

    Modification du fichier entrainement_git.py

---
+++ entrainement_git.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+
+x=5 
+
+
+import matplotlib.pyplot as plt
+# Données (exemple : évolution des ventes)
+x = [1, 2, 3, 4, 5]  # Mois
+y = [10, 15, 7, 12, 20]  # Ventes
+# Création du graphique
+plt.plot(x, y, marker='o', linestyle='-', color='b', label="Ventes")
+# Ajout de titres et labels
+plt.xlabel("Mois")
+plt.ylabel("Nombre de ventes")
+plt.title("Évolution des ventes")
+plt.legend()  # Affiche la légende
+# Affichage du graphique
+plt.show()
+
+'Etape 2'
+'Modification'
+x=5
